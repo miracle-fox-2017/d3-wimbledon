6af865b7713df71d4503c8f1051a8c56580b862b
ameliarahman <amel.rahman5@gmail.com>
2017-12-12 04:34:43 +0700

    adding axis left tapi masih kebalik

--- script.js
+++ script.js
@@ -11,17 +11,15 @@ let svg = d3.select('#results')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
-
+  .append('g')
+  .attr('transform', `translate(${marginLeft}, ${margin})`)
 
 // Data reloading
 let reload = () => {
-  let data = []
+  
   // Your data parsing here..
   d3.tsv('afcw-results.tsv', (rows) => {
-    rows.forEach(function (row) {
-      data.push(row.GoalsScored)
-    })
-    redraw(data)
+    redraw(rows)
   })
 
 }
@@ -29,31 +27,44 @@ let reload = () => {
 
 
 // redraw function
-let redraw = (data) => {
+let redraw = (rows) => {
+  let data = []
+  rows.forEach(function (row) {
+    data.push(row.GoalsScored)
+  })
 const yScale = d3.scaleLinear()
       .domain([0, d3.max(data)])
       .range([0, 300])
 
-const colorScale = d3.scaleLinear()
-      .range(['teal', 'teal'])
+const xScale = d3.scaleLinear()
+      .domain([0, data.length])
+      .range([0, width-margin])
 
-
-      
 svg.selectAll('rect')
       .data(data)
       .enter()
       .append('rect')
+      .attr('height', 0)
       .attr('x', (d, i)=> {
         return i*15
       })
       .attr('y', (d)=> {
         return 300 - yScale(d)
       })
+      .transition()
+      .duration(400)
+      .delay((d, i) => { 
+        return i * 100 
+      })
       .attr('width', (width/data.length)-5)
       .attr('height', (d) => {
         return yScale(d)
       })
-      .attr('fill', colorScale)
+      .attr('fill','teal')
+
+      svg.append('g')
+      .attr('transform', 'translate(0, 10)')
+      .call(d3.axisLeft(yScale).tickValues(d3.range(0,  d3.max(data))))
 }
 
 reload()
