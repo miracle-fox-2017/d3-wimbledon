26049ffac912b7541896914b75e2278d14f11e36
Nandira Paturohman <nandirasp@gmail.com>
2017-12-11 12:18:07 +0700

    release 0 - 1

--- script.js
+++ script.js
@@ -11,16 +11,42 @@ let svg = d3.select('#results')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
-
+  .style('background', '#cacaca')
+  
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv("afcw-results.tsv", function(data) {
+    redraw(data);
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  const multipler = 50
+  const dataValue = data.map(d => d.GoalsScored)
+  console.log(dataValue)
+  
+  const yScale = d3.scaleLinear()
+                   .domain([0, d3.max(dataValue)])
+                   .range([0, height])
 
+  const xScale = d3.scaleLinear()  
+              .domain([0, d3.max(dataValue)])
+              .range([0, width]);
+
+  svg.selectAll('rect')
+     .data(dataValue)
+     .enter()
+     .append('rect')
+     .attr('x', (d, i) => {
+       return i * 16
+      })
+     .attr('width', 15)
+     .attr('height', (d) => {
+       return yScale(d)
+     })
 }
 
 reload()
