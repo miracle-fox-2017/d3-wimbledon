faa55979cd9e3691bdafbc1cc966bbf1beacb856
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-12-11 16:14:12 +0700

    -release 3

--- script.js
+++ script.js
@@ -15,12 +15,59 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  let data = []
+  d3.tsv("afcw-results.tsv", function(inputData) {
+    inputData.forEach(element => {
+      data.push(element.GoalsScored)
+    })
+    redraw(data)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
-  // Your data to graph here
 
+  const yScale = d3.scaleLinear()
+                   .domain([d3.max(data), 0])
+                   .range([300, 0])
+
+  const xScale = d3.scaleLinear()
+                   .domain([0, data.length])
+                   .range([50, width-10])
+
+  const xAxis = d3.axisBottom(xScale)
+                  .ticks(data.length)
+
+  const yAxis = d3.axisLeft(yScale)
+                  .ticks(d3.max(data))
+
+
+  svg.selectAll('rect')
+     .data(data)
+     .enter()
+     .append('rect')
+     .attr('transform', 'translate(0, -20)')
+     .attr('x', (d, i)=> {
+        return (i * 15)+50                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+     })
+     .attr('y', (d)=> {
+        return 300 - yScale(d)
+     })
+     .attr('width', width / data.length-2)
+     .attr('height', (d) => {
+        return yScale(d)
+     })
+     .attr('fill', 'teal')
+
+  svg.append('g')
+     .attr('transform', 'translate(0, 280)')
+     .call(xAxis)
+
+  svg.append('g')
+     .attr('height', 200)
+     .attr('transform', 'translate(50, 25)')
+     .call(yAxis)
 }
 
 reload()
+
