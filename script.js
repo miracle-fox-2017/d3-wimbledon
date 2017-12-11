7ab37eef62dfd4cdd632ba350907315cd1bbf0c3
ameliarahman <amel.rahman5@gmail.com>
2017-12-11 11:56:01 +0700

    Release 1

--- script.js
+++ script.js
@@ -4,23 +4,44 @@
 const width = 750,
   height = 300,
   margin = 20
-marginLeft = 40
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
-
+  .style('background', '#cacaca')
 // Data reloading
 let reload = () => {
-  // Your data parsing here...
+  let data = []
+  // Your data parsing here..
+  d3.tsv('afcw-results.tsv', (rows) => {
+    rows.forEach(function (row) {
+      data.push(row.GoalsScored)
+    })
+    redraw(data)
+  })
+
 }
 
 // redraw function
 let redraw = (data) => {
-  // Your data to graph here
-
+  svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d, i)=> {
+        return i * 5
+      })
+      .attr('y', (d)=> {
+        return 300-d * 10
+      })
+      .attr('width', 20)
+      .attr('height', (d) => {
+        return d * 10
+      })
 }
 
 reload()
