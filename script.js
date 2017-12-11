685e84a0f9e6caef22a66e965630d29b33beebcb
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2017-12-11 15:09:55 +0700

    Axis Done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* global d3 */
-
 // Our canvas
 const width = 750,
   height = 300,
@@ -30,18 +29,22 @@ let redraw = (data) => {
   let goalScore = data.map((d) => {
     return d.GoalsScored
   })
-
+  // Titik Y
   const yScale = d3.scaleLinear()
-  .domain([0, d3.max(goalScore)])
-  .range([0, height])
-
+  .domain([0, d3.max(goalScore),0]) //chart tinggi
+  .range([0, height-30])
+  // Titik X
   const xScale = d3.scaleLinear()
-  .domain([0, goalScore.length])
+  .domain([0, goalScore.length]) // 0-45
   .range([0, width])
 
   const colorScale = d3.scaleLinear()
   .domain([0, d3.max(goalScore)])
   .range(['peru','teal'])
+
+  const axisScaleY = d3.scaleLinear()
+  .domain([0, d3.max(goalScore)])
+  .range([280, 10])
 
   svg.selectAll('rect')
   .data(goalScore)
@@ -49,17 +52,28 @@ let redraw = (data) => {
   .append('rect')
   .attr('class','bar')
   .attr('x', (d,i) => {
-    return i * 25
+    return i * 25+25 //ditambah 25 karena dimulai dari 25
   })
   .attr('y', (d) => {
-    // return 300 - yScale(d)
-    return 0
+    // Graphic dari bawah
+    return 280 - yScale(d) //280 karena dimulai dari 280
+    // return 0  // grafic dimulai dari atas
   })
   .attr('width', 20)
   .attr('height', (d) => {
     return yScale(d)
   })
   .attr('fill', colorScale)
+  // kasih rules
+  let axisY = d3.axisLeft().ticks(4).scale(axisScaleY)
+  let axisX = d3.axisBottom().ticks(42).scale(xScale)
+  svg.append('g')
+  .attr('transform', 'translate(25, 0)') //dimulai dari 25
+  .call(axisY)
+  svg.append('g')
+  .attr('transform', 'translate(25, 280)') //dimulai dari 280
+  .call(axisX)
+
 }
 
 reload()
