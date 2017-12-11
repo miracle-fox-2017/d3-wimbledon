9e12db5de7a962e64d64dc68a1d315a2d4ba7b78
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-12-12 04:30:09 +0700

    Transition Done

--- script.js
+++ script.js
@@ -20,18 +20,43 @@ let reload = () => {
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
-  console.log(data);
-  svg.selectAll('rect')
-    .data(data)
+  const scores=data.map((score)=>{
+    return parseInt(score.GoalsScored)
+  });
+  const xScale=d3.scaleLinear().domain([0,scores.length]).range([0,width]);
+  const yScale=d3.scaleLinear().domain([d3.min(scores),d3.max(scores)]).range([0,300]);
+  const yScaleReverse=d3.scaleLinear().domain([d3.max(scores),d3.min(scores)]).range([0,300]);
+  const colorScale=d3.scaleLinear().domain([d3.min(scores),d3.max(scores)]).range(['red','blue']);
+  const axis=d3.axisLeft(yScale());
+  svg.style('padding-left', '30px')
+    .style('padding-bottom','30px')
+    .style('padding-top','10px')
+    .selectAll('rect')
+    .data(scores)
     .enter()
     .append('rect')
     .attr('width','10px')
     .attr('height',function(score){
-      return score.GoalsScored * 80+'px'
+      return 0
+      // return yScale(score)
     })
     .attr('x',function(score,i){
       return i * 12
-    });
+    })
+    .attr('y',function(score,i){
+      return height - yScale(score)
+    })
+    .attr('fill',colorScale)
+    .transition()
+    .duration(2000)
+    .attr('height',function(score){
+      return yScale(score)
+    })
+  svg.append('g')
+    .call(d3.axisLeft(yScaleReverse))
+    .append('g')
+    .call(d3.axisBottom(xScale))
+    .attr('transform','translate(0,300)');
 }
 
 reload()
