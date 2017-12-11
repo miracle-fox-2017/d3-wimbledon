59f209ffb7257130a30121959f9a18fd72a0a93a
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-12-11 11:22:12 +0700

    Release 0

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 /* global d3 */
 
 // Our canvas
-const width = 750,
-  height = 300,
-  margin = 20
-marginLeft = 40
+const width = 750, height = 300, margin = 20, marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,12 +12,15 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('./afcw-results.tsv',(rows)=>{
+    redraw(rows);
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
-
+  console.log(data);
 }
 
 reload()
