d1bac44bd9db62fe846d8aa45eb245591b8abdaa
wisnudj <jayalahwisnu@gmail.com>
2017-12-11 15:05:32 +0700

    release 2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const width = 750,
   height = 300,
   margin = 20
-marginLeft = 40
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,12 +15,45 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  let data = []
+
+  d3.tsv('afcw-results.tsv', (rows) => {
+    console.log(rows)
+    rows.forEach((row) => {
+      data.push(row.GoalsScored)
+    })
+    redraw(data)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  let dataset = data
 
+  const yScale = d3.scaleLinear()
+      .domain([0, d3.max(dataset)])
+      .range([0, 300])
+
+  const colorScale = d3.scaleLinear()
+      .range(['teal', 'teal'])
+
+  svg.selectAll('rect')
+     .data(dataset)
+     .enter()
+     .append('rect')
+     .attr('class', 'bar')
+     .attr('x', (d, i) => {
+       return i * 15
+     })
+     .attr('y', (d) => {
+       return 300 - yScale(d)
+     })
+     .attr('width', 13)
+     .attr('height', (d) => {
+       return yScale(d)
+     })
+     .attr('fill', colorScale)
 }
 
 reload()
