dbd6e5c5c397b142fba93c5068550ea38240614f
Faris Widyantho <radenfaris@gmail.com>
2017-12-11 11:30:55 +0700

    show afcw result

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // Our canvas
 const width = 750,
   height = 300,
-  margin = 20
-marginLeft = 40
+  margin = 20,
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,11 +15,32 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv("afcw-results.tsv", function(data) {
+    // use data here
+    let result = data.map(d => d.GoalsScored)
+    redraw(result)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('id', 'result')
+    .attr('x', (d, i) => {
+      return i * (margin + 5)
+    })
+    .attr('y', (d) => {
+      return height - d * 20
+    })
+    .attr('width', margin)
+    .attr('height', (d) => {
+      return d * 50
+    })
+
 
 }
 
