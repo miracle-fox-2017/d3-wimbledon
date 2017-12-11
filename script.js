b57f74d9af0a8e7a02eb25fceb4bfd67612d248e
chandrabuwana <cbuwana@gmail.com>
2017-12-11 23:16:54 +0700

    bar done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* global d3 */
 
 // Our canvas
-const width = 750,
+const width = 1000,
   height = 300,
   margin = 20
 marginLeft = 40
@@ -9,16 +9,13 @@ marginLeft = 40
 // Drawing area
 let svg = d3.select('#results')
   .append('svg')
-  .attr('width', width)
+  .attr('width', width + 40)
   .attr('height', height)
 
 // Data reloading
 let reload = () => {
-  let data = []
-  d3.tsv('afcw-results.tsv',(rows)=>{
-    this.data = rows
-    redraw (this.data)
-    // console.log(rows)
+  const isi =d3.tsv('afcw-results.tsv',(rows)=>{
+    redraw (rows)
   })
   // Your data parsing here...
 }
@@ -26,38 +23,52 @@ let reload = () => {
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
-  let dataGoals = data.map((d)=>{
-    return d.GoalsScored
-  })
+  const colorScale = d3.scaleLinear()
+  .domain([0, d3.max(data.map(element =>element.GoalsScored))])
+  .range(['peru','teal'])
+  
   const yScale = d3.scaleLinear()
-  .domain([0,d3.max(dataGoals)])
+  .domain([0,d3.max(data.map(element=>element.GoalsScored))])
   .range([0,height])
 
   const xScale = d3.scaleLinear()
-  .domain([0,d3.max(dataGoals)])
+  .domain([0,data.length])
   .range([0,width])
-  
-  const colorScale = d3.scaleLinear()
-  .domain([0, d3.max(dataGoals)])
-  .range(['peru','teal'])
 
-  svg.selectAll('rect')
-    .data(dataGoals)//ini dari dataGoals yg sudah di map
-    .enter()
-    .append('rect')
-    .attr('class','bar')
-    .attr('x',(d,i)=>{
-      // console.log(d);
-      return i * 25
-    })
-    .attr('y',(d)=>{
-      return 300 - yScale(d)
-    })
-    .attr('width', 20)
-    .attr('height',(d)=>{
-      return yScale(d)
-    })    
-    .attr('fill',colorScale)
+  const yScaleAxis = d3.scaleLinear()
+  .domain([d3.max(data.map(element =>element.GoalsScored)),0])
+  .range([0,300])
+
+  const x_axis = d3.axisBottom(xScale)
+
+  const y_axis = d3.axisLeft(yScaleAxis)
+    
+  let yAxisGroup = svg.append('g')
+    .attr('transform',`translate(40,-35)`)
+    .call(y_axis)
+
+  let xAxisGroup =svg.append('g')
+  .attr('transform',`translate(40, ${height -15})`)
+  .call(x_axis)
+
+  
+
+ let bar= svg.selectAll('rect')
+    .data(data)
+      .enter()
+      .append('rect')
+      .attr('transform','translate(40,-15)')
+      .attr('class','bar')
+      .attr('width',20)
+      .attr('x',(d,i)=>xScale(i))
+      .attr('y',(d)=>height)
+      .attr('heigth',0)
+      .transition()
+        .duration(700)
+        .ease(d3.easeLinear)
+        .attr('y', (d) => height - yScale(d.GoalsScored))
+        .attr('height', d => yScale(d.GoalsScored))
+        .attr('fill', d => colorScale(d.GoalsScored))
 }
 
 reload()
