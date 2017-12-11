d017d73831133de051c22d557d6d3a73b90b59fe
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2017-12-11 11:46:16 +0700

    Nampilin Chart

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const width = 750,
   height = 300,
   margin = 20
-marginLeft = 40
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,12 +15,41 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  let datascore = []
+  d3.tsv("afcw-results.tsv", (data) => {
+    let datascore = data
+    redraw(datascore)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
 
+  // DATA SCORE-NYA
+  let goalScore = data.map((d) => {
+    return d.GoalsScored
+  })
+
+  // const yScale = d3.scaleLinear()
+  // .domain([0, d3.max(datascore)])
+  // .range([0, height])
+
+  svg.selectAll('rect')
+  .data(goalScore)
+  .enter()
+  .append('rect')
+  .attr('class','bar')
+  .attr('x', (d,i) => {
+    return i * 25
+  })
+  .attr('y', (d) => {
+    return 300 - d * 20
+  })
+  .attr('width', 20)
+  .attr('height', (d) => {
+    return d * 20
+  })
 }
 
 reload()
