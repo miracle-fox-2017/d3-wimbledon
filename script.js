01aae54db936777c14933d059f9583a1fabe8023
wisnudj <jayalahwisnu@gmail.com>
2017-12-12 08:17:41 +0700

    menampilkan angka

--- script.js
+++ script.js
@@ -1,59 +1,84 @@
+
 /* global d3 */
 
 // Our canvas
 const width = 750,
-  height = 300,
-  margin = 20
-  marginLeft = 40
+height = 300,
+margin = 20
+marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
+.append('svg')
+.attr('width', width)
+.attr('height', height)
 
 // Data reloading
 let reload = () => {
-  // Your data parsing here...
-  let data = []
+// Your data parsing here...
+let data = []
 
-  d3.tsv('afcw-results.tsv', (rows) => {
-    console.log(rows)
-    rows.forEach((row) => {
-      data.push(row.GoalsScored)
-    })
-    redraw(data)
+d3.tsv('afcw-results.tsv', (rows) => {
+  console.log(rows)
+  rows.forEach((row) => {
+    data.push(row.GoalsScored)
   })
+  redraw(data)
+})
 }
 
 // redraw function
 let redraw = (data) => {
-  // Your data to graph here
-  let dataset = data
+// Your data to graph here
+let dataset = data
 
-  const yScale = d3.scaleLinear()
-      .domain([0, d3.max(dataset)])
-      .range([0, 300])
+const yScale = d3.scaleLinear()
+.domain([0, d3.max(data)])
+.range([height - 50, 0 ])
 
-  const colorScale = d3.scaleLinear()
-      .range(['teal', 'teal'])
+const xScale =
+d3.scaleLinear()
+.domain([0, dataset.length])
+.range([50, width - 50])
 
-  svg.selectAll('rect')
-     .data(dataset)
-     .enter()
-     .append('rect')
-     .attr('class', 'bar')
-     .attr('x', (d, i) => {
-       return i * 15
-     })
-     .attr('y', (d) => {
-       return 300 - yScale(d)
-     })
-     .attr('width', 13)
-     .attr('height', (d) => {
-       return yScale(d)
-     })
-     .attr('fill', colorScale)
+const bottomAxis = 
+d3.axisBottom(xScale)
+.ticks(dataset.length + 1)
+
+const leftAxis = 
+d3.axisLeft(yScale)
+.ticks(d3.max(data))
+
+const colorScale = d3.scaleLinear()
+    .range(['teal', 'teal'])
+
+svg.selectAll('rect')
+   .data(dataset)
+   .enter()
+   .append('rect')
+   .attr('class', 'bar')
+   .attr('x', (d, i) => {
+     return i * 14.24
+   })
+   .attr('y', (d) => {
+     return 300 - yScale(d)
+   })
+   .attr('width', 10)
+   .attr('height', (d) => {
+     return yScale(d)
+   })
+   .attr('fill', colorScale)
+   .attr('transform', 'translate(50, -25)')
+
+   svg.append('g')
+   .attr('transform', 'translate(0, 275)')
+   .call(bottomAxis)
+ 
+   svg.append('g')
+   .attr('height', 200)
+   .attr('transform', 'translate(50, 25)')
+   .call(leftAxis)   
 }
+
 
 reload()
